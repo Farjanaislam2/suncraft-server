afd04969ee79b257efd1558d17184e374fc9031f
Farjanaislam2 <farjanaanika2gmail.com>
2023-06-23 12:32:07 +0600

    add server

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express=require('express');
 const cors = require('cors');
+const cloudinary=require('cloudinary').v2
+const fileUpload=require("express-fileupload")
+
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const port =process.env.PORT || 5000;
+
 
 const app=express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({
+  useTempFiles: true,
+  // limits: {fileSize: 50 *2024 *1024}
+}))
+
+//cloudinary config
+
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_NAME, 
+  api_key:process.env.CLOUDINARY_KEY, 
+  api_secret:process.env.CLOUDINARY_SECRET 
+});
+
+
 
 
 
@@ -27,7 +47,11 @@ async function run() {
   try {
     const imageCollection =client.db('suncraft').collection('suncraftImage')
     const logoCollection =client.db('suncraft').collection('addedlogo')
-    const tableDataCollection =client.db('suncraft').collection('addedTableData')
+    const customerLogoCollection =client.db('suncraft').collection('addedCustomerLogo')
+    const tableDataCollection =client.db('suncraft').collection('allTableData')
+    const aboutDataCollection=client.db('suncraft').collection('about')
+    const aboutCollection=client.db('suncraft').collection('aboutData')
+    const consultingDataCollection=client.db('suncraft').collection('consulting')
 
     // home image post & get
 
@@ -60,26 +84,115 @@ res.send(getLogo)
 })
 
 
+//logo add & get customer
 
+app.post('/addCustomerLogo', async(req,res) =>{
+  const add = req.body
+  console.log(add)
+  const result = await customerLogoCollection.insertOne(add);
+  res.send(result)
+})
+
+app.get('/getCustomerLogo', async(req,res)=>{
+const query = {}
+const getLogo = await customerLogoCollection.find(query).toArray();
+res.send(getLogo)
+})
 
 
 //table data add & get
 
-app.post('/addLogo ', async(req,res) =>{
-  const add = req.body
-  console.log(add)
-  const result = await logoCollection.insertOne(add);
+app.post('/addTableData', async(req,res) =>{
+  const addData = req.body
+  console.log(addData)
+  const result = await tableDataCollection.insertOne(addData);
   res.send(result)
 })
 
-app.get('/getLogo', async(req,res)=>{
+
+app.get('/getTableData', async(req,res)=>{
 const query = {}
-const getLogo = await logoCollection.find(query).toArray();
-res.send(getLogo)
+const result = await tableDataCollection.find(query).toArray();
+res.send(result)
 })
-  } 
 
 
+//About text and image
+
+app.post('/addAbout', async(req,res) =>{
+  const about = req.body
+  console.log(about)
+  const result = await aboutDataCollection.insertOne(about);
+  res.send(result)
+})
+
+
+app.get('/getAbout', async(req,res)=>{
+const query = {}
+const result = await aboutDataCollection.find(query).toArray();
+res.send(result)
+})
+
+
+  //About us video
+  
+app.post('/addAboutData', async(req,res) =>{
+  const imgVideo = req.files.video;
+  const result = await cloudinary.uploader.upload(imgVideo.tempFilePath,{
+    public_id: `${Date.now()}`,
+    resource_type:"auto",
+    folder: "videos"
+  });
+
+  // this is cloudinary response
+  console.log(result,'=====cloudinary============');
+  // res.send(result)
+
+
+
+     // Save the uploaded file metadata to MongoDB
+     const media = {
+      publicId: result.public_id,
+      format: result.format,
+      url: result.secure_url,
+      type: result.resource_type,
+      // Set other metadata fields as needed
+    };
+
+    const saveabout=await aboutCollection.insertOne(media)
+
+    console.log(saveabout,'=====saveabout============');
+
+      // Return the saved media metadata as the response
+      res.send(saveabout)
+})
+
+
+app.get('/getAboutData', async(req,res)=>{
+const query = {}
+const getAboutData = await aboutCollection.find(query).toArray();
+res.send(getAboutData)
+})
+
+
+
+//consulting data
+app.post('/consulting', async(req,res) =>{
+  const consulting = req.body
+  console.log(consulting)
+  const result = await consultingDataCollection.insertOne(consulting);
+  res.send(result)
+})
+
+
+app.get('/getConsulting', async(req,res)=>{
+const query = {}
+const result = await consultingDataCollection.find(query).toArray();
+res.send(result)
+})
+
+
+   }
 
   finally {
     
